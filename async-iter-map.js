c5c1d319c4c17a21ab9b277112f71724932aca7a
jauntywunderkind <jaunty+wunder+kind+scm@eldergods.com>
2020-01-09 19:56:53 -0500

    uhhh wow did i just write next? maybe

--- async-iter-map.js
+++ async-iter-map.js
@@ -2,7 +2,13 @@
 import shimToPolyfill from "shim-to-polyfill"
 const AbortError= ShimToPolyfill( "AbortError", "abort-controller/dist/abort-controller.mjs")
 
-export const DropItem= Symbol.for( "async-iter-map:drop-item")
+export const
+	DropItem= Symbol.for( "async-iter-map:drop-item"),
+	FlattenItem= Symbol.for( "async-iter-map:flatten-item")
+	Item= {
+		Drop: DropITem,
+		Flatten: FlattenItem
+	}
 
 export function AsyncIterMap( input, map, opts){
 	this.abort= this.abort.bind( this)
@@ -16,11 +22,73 @@ export function AsyncIterMap( input, map, opts){
 		},
 		_map: {
 			value: map
+		},
+		_queued: { // iterator for any pending, flattened results
+			value: null
 		}
 	})
 }
 
 AsyncIterMap.prototype.next= async function(){
+	// process all flattened items first
+	QUEUED: if( this._queued){
+		const value= await this._queued.next()
+		if( value.done){
+			break QUEUED
+		}
+		return {
+			value,
+			done: false
+		}
+	}
+
+	// get next item
+	const next= await this._input.next()
+	if( next.done){
+		// no more input data
+		this.done= true
+		return {
+			value: undefined,
+			done: true
+		}
+	}
+
+	// map data
+	const mapped= this._map( next.value)
+
+	// drop?
+	// TODO: yo i heard you don't like recursive calls in non-tail-recursive langs but ohwell
+	if( mapped=== DropItem){
+		// typically would await to give you your useful stack trace but yo maybe some day: tail-recursive
+		return this.next()
+	}
+
+	// flatten
+	if( mapped.value&& mapped.value[ FlattenItem]){
+		let
+			iter,
+			firstVal
+		if( map.value[ Symbol.asyncIterator]){
+			iter= map.value[ Symbol.asyncIterator]()
+			firstVal= await iter.next()
+		}else if( map.value[ Symbol.iterator]){
+			iter= map.value[ Symbol.iterator]()
+			firstVal= iter.next()
+		}
+		if( !iter){
+			throw new Error( "map was told to flatten a value but not iterable")
+		}
+		this._queued= iter
+		return {
+			value: firstVal,
+			done: false
+		}
+	}
+
+	return {
+		value: mapped.value,
+		done: false
+	}
 	
 }
 AsyncIterMap.prototype.return= function(){
