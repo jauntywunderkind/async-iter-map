d268d385c69b70a81b8044d8c0037effbf15b1ac
jauntywunderkind <jaunty+wunder+kind+scm@eldergods.com>
2020-01-29 23:38:39 -0500

    round out drop tests

--- .test/drop.js
+++ .test/drop.js
@@ -26,13 +26,45 @@ tape( "input can have some DropItem", async function( t){
 	t.end()
 })
 
-tape( "input of a single DropItem", function( t){
+tape( "input of a single DropItem", async function( t){
+	t.plan( 1)
+	function map( n){
+		t.fail( "should not map")
+	}
+	const input= (async function*(){
+		yield DropItem
+	})()
+	const m= new Map({ input, map})
+
+	for await( let item of m){
+		t.fail( "should not map")
+	}
+	t.pass( "dropped")
 	t.end()
 })
 
+tape( "map can drop", async function( t){
+	t.plan( 2)
+	let loops= 0 // loop counter
+	function map( n){
+		if( ++loops %2=== 0){
+			return DropItem
+		}
+		return n
+	}
+	const input= (async function*(){
+		yield 1
+		yield 2
+		yield 3
+		yield 4
+	})()
+	const m= new Map({ input, map})
 
-tape( "map can drop", function( t){
+	let agg= 0
+	for await( let item of m){
+		agg+= item
+	}
+	t.equal( loops, 4, "loops=4")
+	t.equal( agg, 4, "agg=4")
 	t.end()
 })
-
-
